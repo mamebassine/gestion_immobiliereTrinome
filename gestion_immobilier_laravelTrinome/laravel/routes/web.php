600113be6e8a105062a439b85947da6e487feb60
Alphahime <alphandiaye383@gmail.com>
2024-06-04 17:54:14 +0200

    ajout des commmentaires

--- gestion_immobilier_laravelTrinome/laravel/routes/web.php
+++ gestion_immobilier_laravelTrinome/laravel/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentaireController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/commentaires', [CommentaireController::class, 'lireCommentaires'])->name('lireCommentaire');
+Route::get('/ajouter', [CommentaireController::class, 'ajoutCommentaire'])->name('ajoutCommentaire');
+Route::post('/traitementAjout', [CommentaireController::class, 'traitementAjoutCommentaire'])->name('commentaireAjouter');
+Route::get('/modifier', [CommentaireController::class, 'modifier']);
+Route::post('/traitementMiseAjour', [CommentaireController::class, 'traitementMiseAjour'])->name('miseAjourTraitement');
+Route::get('/supprimerCommentaire{id}', [CommentaireController::class, 'supprimerComment{id}'])->name('supprimmerCommentaire');
